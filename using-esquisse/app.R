583b1bde437e753a874af416414af294a75c0264
jyuu <jyu21@ncsu.edu>
2019-08-16 15:56:49 -0400

    Testing esquisse

---
+++ using-esquisse/app.R
@@ -0,0 +1,62 @@
+library(esquisse)
+
+ui <- fluidPage(
+  
+  titlePanel("Use esquisse as a Shiny module"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      radioButtons(
+        inputId = "data", 
+        label = "Data to use:", 
+        choices = c("iris", "mtcars"),
+        inline = TRUE
+      )
+    ),
+    mainPanel(
+      tabsetPanel(
+        tabPanel(
+          title = "esquisse",
+          esquisserUI(
+            id = "esquisse", 
+            header = FALSE, # dont display gadget title
+            choose_data = FALSE # dont display button to change data
+          )
+        ),
+        tabPanel(
+          title = "output",
+          verbatimTextOutput("module_out")
+        )
+      )
+    )
+  )
+)
+
+
+server <- function(input, output, session) {
+  
+  data_r <- reactiveValues(data = iris, name = "iris")
+  
+  observeEvent(input$data, {
+    if (input$data == "iris") {
+      data_r$data <- iris
+      data_r$name <- "iris"
+    } else {
+      data_r$data <- mtcars
+      data_r$name <- "mtcars"
+    }
+  })
+  
+  result <- callModule(
+    module = esquisserServer,
+    id = "esquisse",
+    data = data_r
+  )
+  
+  output$module_out <- renderPrint({
+    str(reactiveValuesToList(result))
+  })
+  
+}
+
+shinyApp(ui, server)
